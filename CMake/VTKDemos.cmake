7c5dca5e1a0c95ff19cd2db2c68b33c773e837e5
Juan Hernando <jhernando@fi.upm.es>
2013-10-16 16:49:30 +0200

    Simpler VTKDemos.cmake

--- CMake/VTKDemos.cmake
+++ CMake/VTKDemos.cmake
@@ -1,14 +1,8 @@
 set(VTKDEMOS_PACKAGE_VERSION 1.0.0)
-set(VTKDEMOS_DEPENDS REQUIRED Equalizer Boost VTK)
-
-set(VTKDEMOS_ROOT_VAR VTKDEMOS_ROOT)
 #set(VTKDEMOS_REPO_URL https://github.com/hernando/VTKDemos.git)
 set(VTKDEMOS_REPO_URL ssh://git@bb4.cesvima.upm.es/VTKDemos.git)
-set(VTKDEMOS_REPO_TAG master)
-
-set(VTKDEMOS_FORCE_BUILD ON)
-set(VTKDEMOS_SOURCE ${CMAKE_SOURCE_DIR})
-
+set(VTKDEMOS_DEPENDS REQUIRED Equalizer Boost VTK)
+set(VTKDEMOS_FORCE_BUILD {CI_BUILD})
 
 
 
